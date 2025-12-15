54c168c92e530be46f59171d792dda4521c53c2f
Christopher Zalapa <63930010+ChristopherZalapa@users.noreply.github.com>
2025-12-15 11:05:28 -0800

    Added supabase and updated json file

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 import express from "express";
+import dotenv from "dotenv";
 import cors from "cors";
+import { createClient } from "@supabase/supabase-js";
 
-const PORT = 8000;
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 8000;
 
 app.use(cors());
+
+const supabase = createClient(
+	process.env.SUPABASE_URL,
+	process.env.SUPABASE_ANON_KEY,
+);
 
 app.get("/test", (req, res) => {
 	res.json({ message: "This is a test" });
