9595ede5bcdff5a818a6a53b493fb91260257bdb
Christopher Zalapa <63930010+ChristopherZalapa@users.noreply.github.com>
2025-12-15 16:02:25 -0800

    Added routes folder and auth.js file

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import cors from "cors";
 import { createClient } from "@supabase/supabase-js";
 
+import createAuthRoutes from "./routes/auth.js";
+
 dotenv.config();
 
 const app = express();
@@ -10,15 +12,18 @@ const PORT = process.env.PORT || 8000;
 
 app.use(cors());
 
+app.use(express.json());
+
 const supabase = createClient(
 	process.env.SUPABASE_URL,
 	process.env.SUPABASE_ANON_KEY,
 );
 
-app.get("/test", (req, res) => {
-	res.json({ message: "This is a test" });
-});
+const authRoutes = createAuthRoutes(supabase);
+app.use("/auth", authRoutes);
 
 app.listen(PORT, () => {
 	console.log(`Now listening on ${PORT}`);
+	console.log(`Signup: POST http://localhost:${PORT}/auth/signup`);
+	console.log(`Login: POST http://localhost:${PORT}/auth/login`);
 });
